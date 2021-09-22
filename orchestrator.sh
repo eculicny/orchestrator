a5bfeebab8c6234363e31c179af25a4a0d829f04
chris <chris@ulicny.io>
2021-09-21 20:54:50 -0400

    Switched reverse proxy to traefik

--- orchestrator.sh
+++ orchestrator.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# dashy is the only built container
+export NAMECHEAP_API_KEY=$(cat ./.secrets/namecheap_api_key)
+export NAMECHEAP_API_USER=$(cat ./.secrets/namecheap_api_user)
+export DOMAIN=$(cat ./.secrets/domain)
+export DOMAIN_EMAIL=$(cat ./.secrets/domain_email)
+export WIKI_ADMIN=$(cat ./.secrets/wikiadmin)
+
+# dashy is currently the only built container
 docker-compose --env-file .env \
-		-f docker-compose.yaml \
+		-f docker-compose.traefik.yaml \
 		-f dashy/docker-compose.yaml \
 		-f monitoring/docker-compose.yaml \
 		-f navidrome/docker-compose.yaml \
 		-f plex/docker-compose.yaml \
-		up -d --build
+		-f tiddlywiki/docker-compose.yaml \
+		up -d
