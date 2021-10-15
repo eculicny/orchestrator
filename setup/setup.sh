aa2dd1be64ef000d121792567d7cf33a9fd30eaa
chris <chris@ulicny.io>
2021-10-14 21:01:32 -0400

    Added node-exporter for prometheus

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,9 @@
 apt-get update
 
 apt-get install -y git htop ncdu build-essential
+
+# node exporter for prometheus
+# wget https://github.com/prometheus/node_exporter/releases/download/v*/node_exporter-*.*-amd64.tar.gz
+# tar xvfz node_exporter-*.*-amd64.tar.gz
+# cd node_exporter-*.*-amd64
+# ./node_exporter
