7cca0843fc6bc8462c64065f7f15a18cf082a6e0
chris <chris@ulicny.io>
2022-07-31 22:45:26 -0400

    Adjusted setup files

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,9 +1,13 @@
-apt-get update
 
+printf "Updating and installing apt packages\n"
+apt-get update
 apt-get install -y git htop ncdu build-essential
 
-# node exporter for prometheus
-# wget https://github.com/prometheus/node_exporter/releases/download/v*/node_exporter-*.*-amd64.tar.gz
-# tar xvfz node_exporter-*.*-amd64.tar.gz
-# cd node_exporter-*.*-amd64
-# ./node_exporter
+printf "Copying cron job files\n"
+cp -v ./archive_cron /etc/cron.d/
+
+printf "Copying alias file\n"
+cp -v ./00-aliases.sh /etc/profiles.d/
+
+printf "Copying blocklist file\n"
+cp -v ./01-blocklist.conf /etc/rsyslog.d/
